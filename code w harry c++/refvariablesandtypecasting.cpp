9cb789b2e16044275dfd480cd3484c85ae4cd95f
Alok Singh <96775724+Alok57hhj@users.noreply.github.com>
2023-10-30 11:22:32 +0530

    Add files via upload

---
+++ code w harry c++/refvariablesandtypecasting.cpp
@@ -0,0 +1,47 @@
+#include<iostream>
+using namespace std;
+int c=45;
+
+int main()
+{
+    // *****built in data types****
+    // int a,b,c;
+    // cout<<"enter the value of a:\n";
+    // cin>>a;
+    // cout<<"enter the value of b:\n";
+    // cin>>b;
+    // c=a+b;
+    // cout<<"thhe sum is c"<<c <<endl;
+    // cout<<"the global operator c is"<<::c;
+
+    // *******float, duble, and lomg double
+    
+    // float d =34.4f;
+    // long double e =34.4l;
+    // cout<<"the size of 34.4 is "<<sizeof(34.4)<<endl;
+    // cout<<"the size of 34.4f is "<<sizeof(34.4f)<<endl;
+    // cout<<"the size of 34.4l is "<<sizeof(34.4l)<<endl<<endl;
+
+    // cout<<"the value of d is"<<d<<endl;
+    // cout<<"the value of e is"<<e<<endl;
+    
+
+    // reference variables
+
+    // rohan---> monty--->rohu---->dangerous coder
+    // float x = 455;
+    // float & y =x;
+    // cout<<x <<endl;
+    // cout<<y <<endl;
+
+
+    // *****Typecasting*****
+    int a =45;
+    float b = 45.46;
+    cout <<"the  value of a is "<<(float)a;
+    cout<<"the vaiue of b is "<<(int)b;
+
+
+
+    return 0;
+}
